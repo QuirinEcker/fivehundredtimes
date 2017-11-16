98af279f7c89d50d595ef13125580ae4e5485973
QuirinEcker <quirin.ecker@bajupa.com>
2017-11-16 18:54:34 +0100

    initial other files

---
+++ src/main/java/Ecker_TimesFiveHundered.java
@@ -0,0 +1,23 @@
+public class Ecker_TimesFiveHundered {
+
+    public static void main(String[] args) {
+
+        for (int i = 0 ; i < 71;i++){
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.print("Java Rocks|");
+            System.out.println();
+        }
+        System.out.print("Java Rocks|");
+        System.out.print("Java Rocks|");
+        System.out.print("Java Rocks|");
+
+    }
+}
